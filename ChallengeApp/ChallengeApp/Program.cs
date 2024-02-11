d4a3263f6dd30e864ca0a0c9be8e83d4d547aa72
MarcinOlsztyn <marcin81.kowalewski@gmail.com>
2024-02-11 19:51:10 +0100

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Witek";
+string sex = "mężczyzna";
+int age = 15;
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (sex == "mężczyzna" && age < 18)
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+}
+
+
